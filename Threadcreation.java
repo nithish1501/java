2ccfc90ffe2a3b15b8d4b344ca64aa6a4faed5e1
nithish1501 <nithishanish2007@gmail.com>
2026-07-26 10:27:42 +0530

    Add files via upload

---
+++ Threadcreation.java
@@ -0,0 +1,12 @@
+class MyThread extends Thread{
+    public void run(){
+        System.out.println("Thread Running");
+    }
+}
+
+public class ThreadDemo{
+    public static void main(String[] args){
+        MyThread t=new MyThread();
+        t.start();
+    }
+}
