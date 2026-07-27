3e210ec351c1ad9d72a7287e7415b3bd58daa920
nithish1501 <nithishanish2007@gmail.com>
2026-07-27 20:11:35 +0530

    Add files via upload

---
+++ Hashmap.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+class HashMapDemo{
+    public static void main(String[] args){
+        HashMap<Integer,String> map=new HashMap<>();
+
+        map.put(1,"Java");
+        map.put(2,"Python");
+
+        System.out.println(map);
+    }
+}
