581c09f83b38be63889f91e5f08a1fa5c43c41b6
nithish1501 <nithishanish2007@gmail.com>
2026-07-28 19:46:30 +0530

    Add files via upload

---
+++ Haspset.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+class HashSetDemo{
+    public static void main(String[] args){
+        HashSet<Integer> set=new HashSet<>();
+
+        set.add(10);
+        set.add(20);
+        set.add(10);
+
+        System.out.println(set);
+    }
+}
---
+++ Lambda.java
@@ -0,0 +1,12 @@
+ interface Demo{
+    void show();
+}
+
+class LambdaDemo{
+    public static void main(String[] args){
+        Demo d=()->System.out.println("Hello Java");
+        d.show();
+    }
+} {
+    
+}
